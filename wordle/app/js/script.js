e71be6065178ef7c7a4c0bf81b183ed965fac62e
NyMek <nymek.kontakt@gmail.com>
2023-01-25 10:51:12 +0100

    solution guess

--- wordle/app/js/script.js
+++ wordle/app/js/script.js
@@ -5,14 +5,35 @@ let currentGuessCount = 1;
 let currentGuess = document.querySelector('#guess' + currentGuessCount);
 // let currentLetters = currentGuess.dataset.letters;
 
+let words = [
+   'baker','store','horse','speak','clone','apple','bread'];
+   let solutionWord = '';
+
+const chooseWord = () => {
+    let randomItem = (Math.floor(Math.random() * (words.length))) + 1;
+    console.log(randomItem);
+    solutionWord = words[randomItem];
+    console.log(solutionWord);
+}
+chooseWord();
+
 document.addEventListener('keydown', (e) => {
     console.log('keypress ' + e.key);
     console.log (currentGuess);
  let keypress = e.key;
- if(keypress.length == 1 && lettersPattern.test(e.key)) {
+ if(keypress.length == 1 && lettersPattern.test(e.key) && currentGuess.dataset.letters.length <5) {
 
     console.log('is letter ');
     updateLetter(keypress);
+ } else if(e.key == 'Backspace' && currentGuess.dataset.letters != '' ) {
+    console.log('backspace');
+    deleteFromLetters();
+ } else if (e.key == 'Enter' && currentGuess.dataset.letters.length == 5) {
+    console.log('submit guess');
+    for(let i = 0; i<5; i++) {
+        checkLetter(i);
+    }
+
  }
 });
 
@@ -30,4 +51,41 @@ const updateTiles = (tileNumber, letter) => {
 
     currentTile.innerHTML = letter;
     
-}
+};
+
+//backspace delete last letter
+
+const deleteFromLetters = () => {
+    let oldLetters = currentGuess.dataset.letters;
+
+    let newLetters = oldLetters.slice(0, -1);
+    currentGuess.dataset.letters = newLetters;
+    deleteFromTiles(oldLetters.length);
+};
+
+
+const deleteFromTiles = (tileNumber) => {
+    document.querySelector('#guessTile' + tileNumber).innerText ='';
+};
+
+
+//check letter to solution
+
+const checkLetter = (position) => {
+    let guessedLetter = currentGuess.dataset.letters.charAt(position);
+    let solutionLetter = solutionWord.charAt(position);
+
+    console.log( guessedLetter, solutionLetter);
+
+    if(guessedLetter == solutionLetter) {
+        return 'correct';
+    } else {
+        return checkLetterExists(guessedLetter) ? 'present ' : 'absent';
+
+
+    }
+};
+
+const checkLetterExists = (letter) => {
+    solutionWord.includes(letter);
+};
