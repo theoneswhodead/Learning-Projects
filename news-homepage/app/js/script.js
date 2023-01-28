fa2e0abb96339b6e472c6d4bb6418fe66b5640e9
NyMek <nymek.kontakt@gmail.com>
2023-01-28 14:47:56 +0100

    end a news homepage

--- news-homepage/app/js/script.js
+++ news-homepage/app/js/script.js
@@ -1,24 +1,29 @@
 
-console.log('script');
 const body = document.querySelector('body');
 const hamburger = document.querySelector('.header__img');
 const ul = document.querySelector('ul');
 
-console.log(hamburger);
 let toggle = true;
+
 hamburger.addEventListener('click', () => {
-    console.log('asd');
+
     ul.classList.toggle('hide');
+
     if(toggle) {
         hamburger.src="/assets/images/icon-menu-close.svg";
         body.style.background="#807E89";
+        body.style.filter="saturate(33%)"
         body.style.transition="background 250ms ease-in-out";
+        
         toggle = false;
-    } else{
+
+    } else {
         hamburger.src="/assets/images/icon-menu.svg";
         body.style.background="#fff";
-        body.style.transition="background 250ms ease-in-out"
+        body.style.filter="saturate(100%)"
+        body.style.transition="background 250ms ease-in-out";
+
         toggle = true;
     }
 
-})
+});
