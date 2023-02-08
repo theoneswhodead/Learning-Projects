ab1cfd0c919d26d4e8c0a0adde0e735df40b3a81
NyMek <nymek.kontakt@gmail.com>
2023-02-08 12:13:43 +0100

    todo

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,10 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
+const filterOption = document.querySelector('.filter-todo');
 
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 
@@ -44,4 +45,42 @@ function addTodo(event) {
         todoList.appendChild(todoDiv);
 
         todoInput.value = '';
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    //delete todo
+
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        })
+    }
+
+    //check mark
+
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if(todo.classList.contains('completed')) {
+                    todo.style.display ='flex';
+                }else {
+                  todo.style.display = 'none';  
+                }
+        }
+    })
 }
