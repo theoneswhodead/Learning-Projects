6b0503bcdde62989d8f570bbd09057f84402aecb
NyMek <nymek.kontakt@gmail.com>
2023-02-09 17:52:44 +0100

    calc

--- tip-calculator-app-main/app/js/script.js
+++ tip-calculator-app-main/app/js/script.js
@@ -6,3 +6,17 @@ const amount = document.querySelector('.sum__summary-amount');
 const total = document.querySelector('.sum__summary-total');
 
 const reset = document.querySelector('.sum__reset');
+
+ let sum = 0;
+
+
+// bill.addEventListener('keydown', e => {
+
+// })
+
+
+sum = parseFloat(bill.value) / parseFloat(people.value);
+reset.addEventListener('click', () => {
+    total.innerText = `${sum}`;
+})
+console.log(sum);
