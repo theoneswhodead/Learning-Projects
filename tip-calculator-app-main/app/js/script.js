45fe8c9a6860a90267056842e22c6c4f9c5ba7f2
NyMek <nymek.kontakt@gmail.com>
2023-02-09 17:12:31 +0100

    calc

---
+++ tip-calculator-app-main/app/js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelectorAll('.calc__btn');
+const bill = document.querySelector('.bill');
+const people = document.querySelector('.people');
+
+const amount = document.querySelector('.sum__summary-amount');
+const total = document.querySelector('.sum__summary-total');
+
+const reset = document.querySelector('.sum__reset');
