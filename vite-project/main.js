0a265e3599126fc290310b57b97365ebd1ee31f0
NyMek <nymek.kontakt@gmail.com>
2023-03-12 17:23:07 +0100

    edit

--- vite-project/main.js
+++ vite-project/main.js
@@ -10,3 +10,9 @@ navToggle.addEventListener('click', () => {
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-overlay');
 })
+
+
+  const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: true,
+  });
