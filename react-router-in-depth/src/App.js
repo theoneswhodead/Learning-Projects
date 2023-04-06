0d918e738f851ac64a9b5f62c411a2cb42e579c1
NyMek <nymek.kontakt@gmail.com>
2023-04-06 09:48:00 +0200

    article and blog section

--- react-router-in-depth/src/App.js
+++ react-router-in-depth/src/App.js
@@ -1,30 +1,37 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { 
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider} from "react-router-dom";
 
 import Home from './pages/Home';
 import About from "./pages/About";
+import Faq from './pages/help/Faq';
+import Contact from "./pages/help/Contact";
+
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={ <Home /> }/> 
+      <Route path="about" element={ <About /> }/>
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />}/>
+        <Route path="contact" element={<Contact />}/>
+      </Route>
+
+      <Route path="*" element={<NotFound />}/>
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobarouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
 
-        <main>
-          <Routes>
-            <Route path="/" element={ <Home /> }/> 
-            {/* // path="/" to samo co index */}
-            <Route path="about" element={ <About /> }/> 
-            {/* // /about */}
-
-          </Routes>
-        </main>
-    </BrowserRouter>
-
+    <RouterProvider router={router} />
   );
 }
 
---
+++ react-router-in-depth/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export default function NotFound() {
+  return (
+    
+    <div>
+        <h2>Page not found!</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, nulla?</p>
+
+        <p>Go to the <Link to="/">Homepage</Link>.</p>
+    </div>
+  )
+}
