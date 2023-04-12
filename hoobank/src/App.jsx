8f0318aed70a76b94f07b8075796f1ce0164045f
NyMek <nymek.kontakt@gmail.com>
2023-04-12 10:30:24 +0200

    hoobank

---
+++ hoobank/src/App.jsx
@@ -0,0 +1,34 @@
+import styles from "./style";
+
+
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        CardDeal
+        ter 
+        asds
+        CTA
+        footer
+      </div>
+    </div>
+
+  </div>
+);
+
+export default App;
