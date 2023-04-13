bd75dfb266e70e35dc44fed67d10996666e27e1e
NyMek <nymek.kontakt@gmail.com>
2023-04-13 11:19:19 +0200

    stats and hero section

---
+++ crowdfunding-page/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+//import Header from 
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
---
+++ crowdfunding-page/src/components/reward/Reward.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const reward = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reward
