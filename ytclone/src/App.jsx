6c07f86278e4d25312da0d78f5377ec6971008c5
NyMek <nymek.kontakt@gmail.com>
2023-04-19 14:17:37 +0200

    YtClone

--- ytclone/src/App.jsx
+++ ytclone/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+// import './App.css'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components'
 
-function App() {
- 
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+  <Box sx={{backgroundColor: '#000'}}>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Feed />}/>
+      <Route path="/video/:id" element={<VideoDetail/>}/>
+      <Route path="/channel/:id" element={<ChannelDetail/>}/>
+      <Route path="/search/:serchTerm" element={<SearchFeed/>}/>
+    </Routes>
+  </Box>
+
+  </BrowserRouter>
+)
 
 export default App
---
+++ ytclone/src/components/Feed.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
+
+const Feed = () => {
+  return (
+    <Stack sx={{flexDirection: {sx: "column", md: "row"} }}>
+      feed
+    </Stack>
+  )
+}
+
+export default Feed
