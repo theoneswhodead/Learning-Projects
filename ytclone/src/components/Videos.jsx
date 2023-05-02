dc04a6103727ac15c15c39f9044c65f4ffdde8a8
NyMek <nymek.kontakt@gmail.com>
2023-05-02 09:36:21 +0200

    ytclone end

--- ytclone/src/components/Videos.jsx
+++ ytclone/src/components/Videos.jsx
@@ -3,10 +3,10 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
-  console.log(videos)
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return `Loading....`
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction || "row" } flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) => (
         <Box hey={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
