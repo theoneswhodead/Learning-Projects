7846295776fa9829bdb8a0a4bb17758d3f05b309
NyMek <nymek.kontakt@gmail.com>
2023-05-12 13:05:03 +0200

    fitness app yt api

--- fitnessApp/src/components/Exercises.jsx
+++ fitnessApp/src/components/Exercises.jsx
@@ -7,7 +7,35 @@ import ExerciseCard from './ExerciseCard';
 import Loader from './Loader';
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
-  console.log(exercises)
+const [currentPage, setCurrentPage] = useState(1);
+
+const exercisesPerPage = 9;
+
+const indexOfLastExercise = currentPage *exercisesPerPage;
+const indexOfFirstExercises = indexOfLastExercise - exercisesPerPage;
+
+const currentExercises = exercises.slice(indexOfFirstExercises, indexOfLastExercise)
+
+const paginate = (e, value) => {
+  setCurrentPage(value);
+
+  window.scrollTo({ top: 1800, behavior: 'smooth' })
+}
+
+useEffect(() => {
+  const fetchExercisesData = async () => {
+    let exercisesData = [];
+
+    if(bodyPart === 'all') {
+      exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+    } else {
+      exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+    }
+    setExercises(exercisesData);
+  }
+  fetchExercisesData();
+}, [bodyPart])
+
   return (
     <Box id="exercises" sx={{mt: { lg: '110px'}}}
     mt="50px"
@@ -17,9 +45,22 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
         Showing Results
       </Typography>
       <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise}/>
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination 
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )}
       </Stack>
     </Box>
   )
--- fitnessApp/src/utils/fetchData.js
+++ fitnessApp/src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': '7ead1c56eamsh0f94cfe916b2e53p18dc50jsna64ac8e9404d',
   },
 };
 
