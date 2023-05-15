33c5f12b4ebc2faac1fd03bb3419450bbe0a1c50
NyMek <nymek.kontakt@gmail.com>
2023-05-15 13:39:50 +0200

    Fylo

---
+++ fylo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-intro w-full overflow-hidden'>
+      <h1>Hello World</h1>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ fylo/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        intro: "hsl(217, 28%, 15%)",
+        main: "hsl(218, 28%, 13%)",
+        footer: "hsl(216, 53%, 9%)",
+        testimonials: "hsl(219, 30%, 18%)",
+        error: "hsl(0, 100%, 63%)",
+        cyan: "hsl(176, 68%, 64%)",
+        blue: "hsl(198, 60%, 50%)",
+        white: "hsl(0, 0%, 100%)"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
