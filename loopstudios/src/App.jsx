671761d539db644bf6171b6431859a20c0567073
NyMek <nymek.kontakt@gmail.com>
2023-05-17 15:26:58 +0200

    loopstudios

---
+++ loopstudios/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1 className="text-white">Hello World</h1>
+      <h2 className='text-white'>asdasdsad</h2>
+      
+    </div>
+  )
+}
+
+export default App
