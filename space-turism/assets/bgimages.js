35cd8b876b09d2a9b42b1d5e914e7a2c5a67b28b
NyMek <nymek.kontakt@gmail.com>
2023-05-20 13:33:22 +0200

    web3app

--- space-turism/assets/bgimages.js
+++ space-turism/assets/bgimages.js
@@ -1,19 +1,3 @@
-// import HomeDesktopBackground from '../assets/home/background-home-desktop.jpg'
-// import HomeTabletBackground from '../assets/home/background-home-tablet.jpg'
-// import HomeMobileBackground from '../assets/home/background-home-mobile.jpg'
-
-// import DestinationDesktopBackground from '../assets/destination/background-destination-desktop.jpg'
-// import DestinationTabletBackground from '../assets/destination/background-destination-tablet.jpg'
-// import DestinationMobileBackground from '../assets/destination/background-destination-mobile.jpg'
-
-// import CrewDesktopBackground from '../assets/crew/background-crew-desktop.jpg'
-// import CrewTabletBackground from '../assets/crew/background-crew-tablet.jpg'
-// import CrewMobileBackground from '../assets/crew/background-crew-mobile.jpg'
-
-// import TechnologyDesktopBackground from '../assets//technology/background-technology-desktop.jpg'
-// import TechnologyTabletBackground from '../assets//technology/background-technology-tablet.jpg'
-// import TechnologyMobileBackground from '../assets//technology/background-technology-mobile.jpg'
-
 const HomeDesktopBackground = '../assets/home/background-home-desktop.jpg'
 const HomeTabletBackground = '../assets/home/background-home-tablet.jpg'
 const HomeMobileBackground = '../assets/home/background-home-mobile.jpg'
