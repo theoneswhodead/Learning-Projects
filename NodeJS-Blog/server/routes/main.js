9b51150fe6b760e27fd6d70820e6b4fdffb985e3
NyMek <nymek.kontakt@gmail.com>
2023-05-21 08:47:56 +0200

    Main nodeblog

--- NodeJS-Blog/server/routes/main.js
+++ NodeJS-Blog/server/routes/main.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 //Routes 
 router.get('', (req,res) => {
-    res.render('index');
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJS Express & MongoDb."
+    }
+
+    res.render('index', { locals });
 })
+
 
 module.exports = router;
