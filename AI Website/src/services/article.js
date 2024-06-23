ded7e98363860d311b8436c2e7f277e21b986ae3
theoneswhodead <uranowski40@gmail.com>
2024-06-23 13:23:56 +0200

    linux test2

--- AI Website/src/services/article.js
+++ AI Website/src/services/article.js
@@ -15,8 +15,8 @@ export const articleApi = createApi({
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            // encodeURIComponent() function encodes special characters that may be present in the parameter values
-            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            // encodeURIComponent() 
+            
             query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
         }),
     }),
